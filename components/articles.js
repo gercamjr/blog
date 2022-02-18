0b6d561c0b3ac5a7cb2c8a657cfb5f768f6c5d82
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-02-18 10:14:16 -0600

    refactor articles to include the swiper

--- components/articles.js
+++ components/articles.js
@@ -2,36 +2,17 @@ import React from "react";
 import Card from "./card";
 
 const Articles = ({ articles }) => {
-  const leftArticlesCount = Math.ceil(articles.length / 5);
-  const leftArticles = articles.slice(0, leftArticlesCount);
-  const rightArticles = articles.slice(leftArticlesCount, articles.length);
+
+  const cards = articles.map((article) => 
+  <Card article={article} key={`${article.attributes.slug}`} />)
 
   return (
-    <div>
-      <div className="uk-child-width-1-2@s" data-uk-grid="true">
-        <div>
-          {leftArticles.map((article, i) => {
-            return (
-              <Card
-                article={article}
-                key={`article__left__${article.attributes.slug}`}
-              />
-            );
-          })}
+    <div className="blog-slider">
+      
+        <div className="blog-slider__wrp swiper-wrappe">
+          {cards}
         </div>
-        <div>
-          <div className="uk-child-width-1-2@m uk-grid-match" data-uk-grid>
-            {rightArticles.map((article, i) => {
-              return (
-                <Card
-                  article={article}
-                  key={`article__left__${article.attributes.slug}`}
-                />
-              );
-            })}
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 };
