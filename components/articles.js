6b60c5f7dcc63b3a64f2924d85495d8ca3f7516f
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-02-27 19:10:05 -0600

    fixed styling for pagination buttons

--- components/articles.js
+++ components/articles.js
@@ -2,16 +2,14 @@ import React from "react";
 import Card from "./card";
 
 const Articles = ({ articles }) => {
-
+  
   const cards = articles.map((article) => 
   <Card article={article} key={`${article.attributes.slug}`} />)
 
   return (
     <div className="blog-slider">
       
-        <div className="blog-slider__wrp swiper-wrapper">
-          {cards}
-        </div>
+        {articles.length > 0 ? <div className="blog-slider__wrp swiper-wrapper">{cards}</div> : <h2>No articles under this category</h2>}
         <div className="blog-slider__pagination"></div>
     </div>
   );
