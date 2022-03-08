7fa1824271c8e6a1c56d5982e22414e7352f130b
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2022-03-07 21:03:27 -0600

    sorting the articles based on the published date...

--- components/articles.js
+++ components/articles.js
@@ -2,6 +2,9 @@ import React from "react";
 import Card from "./card";
 
 const Articles = ({ articles }) => {
+  articles.sort(function(a,b){
+    return new Date(b.attributes.publishedAt) - new Date(a.attributes.publishedAt)
+  })
   console.log(articles)
   const cards = articles.map((article) => 
   <Card article={article} key={`${article.attributes.slug}`} />)
